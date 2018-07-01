1dffd78be91d1f23fb4f6d29ff12cd5d7e8d81ae
hamger <2313303800@qq.com>
2018-07-01 21:27:51 +0800

    fix option

--- src/stage9/options.js
+++ src/stage9/options.js
@@ -1,39 +1,33 @@
-const R = require('ramda');
-
 export function mergeOptions(parent, child) {
     let options = {}
-
     // 合并 data ，一个函数
     options.data = mergeData(parent.data, child.data)
 
     // 合并 methods 同名覆盖
-    options.methods = R.merge(parent.methods, child.methods)
+    options.methods = Object.assign(parent.methods || {}, child.methods || {})
 
     // 合并 watcher 同名合并成一个数组
     options.watch = mergeWatch(parent.watch, child.watch)
 
     // 合并 computed 同名覆盖
-    options.computed = R.merge(parent.computed, child.computed)
+    options.computed = Object.assign(parent.computed || {}, child.computed || {})
 
     return options
 }
 
 function mergeData(parentValue, childValue) {
-    if (!parentValue) {
-        return childValue
-    }
-    if (!childValue) {
-        return parentValue
-    }
+    if (!parentValue) return childValue
+    if (!childValue) return parentValue
+    
     return function () {
         // parentValue / childValue 是函数
-        return R.merge(parentValue.call(this), childValue.call(this))
+        return Object.assign(parentValue.call(this), childValue.call(this))
     }
 }
 
 function mergeWatch(parentVal, childVal) {
-    if (!childVal) return R.clone(parentVal || {})
-    let ret = R.merge({}, parentVal)
+    if (!childVal) return parentVal || {}
+    let ret = Object.assign({}, parentVal)
     for (let key in childVal) {
         let parent = ret[key]
         let child = childVal[key]
